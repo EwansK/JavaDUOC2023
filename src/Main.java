464c15a37d6804d23f1155846681c454ac8dde04
EwansK <ew.ewoldt@duocuc.cl>
2023-09-03 20:55:42 -0300

    Testeando commits

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author ewans
+ */
+public class Main {
+    public static void main(String[] args) {
+        
+        Tarjeta tarjeta1 = new Tarjeta();
+        
+        tarjeta1.generarCodigo();
+        System.out.println(tarjeta1.getCodigo()); 
+        
+        
+        
+    }
+}
